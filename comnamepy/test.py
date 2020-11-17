1e8a2765f9e1cba9e6b817c0f95c10dcc3c30b0b
Umeno-Akira <chotips08@gmail.com>
2020-11-17 15:45:34 +0900

    [update] #3 update comnamepy

--- comnamepy/test.py
+++
@@ -1,6 +0,0 @@
-from get_company_name.comnamepy import get_comname
-
-if __name__ == '__main__':
-    company = get_comname.whois_search_company('rc.jp')
-    # company = get_comname.get_title('sut.jp')
-    print(company)
